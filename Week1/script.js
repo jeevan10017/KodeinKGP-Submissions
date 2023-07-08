49e499c205b802596759503dd7ff37aef2d4a7b4
Jeevan10017 <jeevankumarkorra2005@gmail.com>
2023-07-08 20:05:48 +0530

    week1 submission

---
+++ Week1/script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("demoButton");
+// getting the button element by its ID 
+button.addEventListener("click",function() {
+alert('Congratulations! You have clicked the Button.');
+});
+// add a click event listener to the button 
